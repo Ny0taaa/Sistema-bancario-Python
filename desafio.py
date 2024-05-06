764713dc5bfd849894e8eb085a4df16625bd8286
Ny0taaa <davicandidope@gmail.com>
2024-05-06 15:18:34 -0300

    Depósito add

--- desafio.py
+++ desafio.py
@@ -8,4 +8,23 @@ menu = '''
     [Q] Sair
 
 '''
-print(menu)
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+limite_saques = 3
+
+while True:
+    
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor a ser depositado: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R${valor: 2.f}\n"
+
+        else:
+            print("Operação falhou. Valor informado é inválido.")
