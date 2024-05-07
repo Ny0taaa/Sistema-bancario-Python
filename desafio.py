092c4bdc322b7b8a4b247a8ce553d03d3841e87c
Ny0taaa <davicandidope@gmail.com>
2024-05-06 22:17:59 -0300

    sacar, extrato e cadastro do usuário add

--- desafio.py
+++ desafio.py
@@ -27,43 +27,56 @@ def depositar(saldo, valor, extrato,/):
 
     return saldo, extrato
 
-def sacar()
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saque = numero_saques >= limite_saques
 
+    if excedeu_saldo:
+         print("Operação Falhou! O valor informado é maior que o seu saldo.")
 
+    elif excedeu_limite:
+        print("Operação Falhou! O valor informado excedeu o limite.")
 
-#    elif opcao == "s": #Saque
+    elif excedeu_saque:
+        print("Operação Falhou! O número máximo de saques excedido.")
 
-#        valor = float(input("Informe o valor do saque: "))
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+        print(f"Saque: R$ {valor:.2f}\n")
+        print("\n===== Operação realizada com sucesso! =====")
 
-        excedeu_saldo = valor > saldo
-        excedeu_limite = valor > limite
-        excedeu_saque = numero_saques >= LIMITE_SAQUES
+    else:
+        print("Operação Falhou! O valor informado é inválido.")
 
-        if excedeu_saldo:
-            print("Operação Falhou! O valor informado é maior que o seu saldo.")
+    return saldo, extrato
 
-        elif excedeu_limite:
-            print("Operação Falhou! O valor informado excedeu o limite.")
+def show_extrato(saldo, /, *, extrato):
+     
+    print("\n=========== EXTRATO ===========")
+    print("Não foram realizados movimentações" if not extrato else extrato)
+    print("\n             ======")
+    print(f"\nSaldo: R$ {saldo: .2f}")
+    print("\n=================================")
 
-        elif excedeu_saque:
-            print("Operação Falhou! O número máximo de saques excedido.")
+def new_usuario(usuarios):
+    cpf = input("Informe o seu CPF: ")
+    usuario = fitrar_usuario(cpf,usuarios)
 
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-            print(f"Saque: R$ {valor:.2f}\n")
-            print("Operação realizada com sucesso!")
-
-        else:
-            print("Operação Falhou! O valor informado é inválido.")
+    if usuario:
+        print("\n===== Já existe um usuário com esse CPF! =====")
     
-    elif opcao == "e": #Extrato
-        print("\n=========== EXTRATO ===========")
-        print("Não foram realizados movimentações" if not extrato else extrato)
-        print("\n             ======")
-        print(f"\nSaldo: R$ {saldo: .2f}")
-        print("\n=================================")
+    nome = input("Digite o seu nome completo: ")
+    data_nasc = input("Digite a sua data de nascimento: ")
+    endereco = input("Digite o seu endereço (logradouro, nº - bairro - cidade/sigla do estado): ")
+
+    usuarios.append({"nome": nome, "data_nasc": data_nasc, "cpf": cpf, "endereco": endereco})
+
+    print("\n===== Usuário cadastrado com sucesso! =====")
+
+
 
     elif opcao == "q": #Sair
         break
